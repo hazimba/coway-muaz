2de6398ec6a4850400c24c6fb8917c1f2a05b707
Hazim Bakar <hazimbaa@gmail.com>
2025-09-26 10:06:08 +0800

    sitemap update 2

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,23 @@
-import { NextResponse } from "next/server";
+import { MetadataRoute } from "next";
 
-export async function GET() {
+export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://muaz-coway.vercel.app";
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>${baseUrl}/</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>0.9</priority>
-    </url>
-    <url>
-        <loc>${baseUrl}/home</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>1</priority>
-    </url>
-    </urlset>`;
-
-  return new NextResponse(xml, {
-    headers: {
-      "Content-Type": "application/xml",
+  // Static pages
+  const staticPages: MetadataRoute.Sitemap = [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
     },
-  });
+    {
+      url: `${baseUrl}/home`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+
+  return staticPages;
 }
